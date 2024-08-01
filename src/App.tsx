9edc69b892f9c76bddf98cfa5ee8db35ba6ba29c
Wongsodillon <Wongsodillon@gmail.com>
2024-08-01 22:41:00 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Landing from "./pages/Landing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
+
+const Landing = () => {
+  return (
+    <div className="min-h-screen relative home-background">
+      <div className="w-64 h-[32rem] border border-white purple-gradient absolute left-[2%] top-0 drop-shadow-2xl rounded-br-full rounded-bl-full z-0"></div>
+      <div className="w-[32rem] h-64 border border-white yellow-gradient absolute bottom-0 right-0 drop-shadow-2xl rounded-tl-full rounded-bl-full z-0"></div>
+      <Navbar />
+      <main className="flex flex-col items-center justify-center text-center pt-32">
+        <div className="flex flex-col gap-2 items-start">
+          <span className="text-[#892EE1] text-7xl font-anton">THEY ARE</span>
+          <span className="text-[#FF9A00] text-8xl font-anton">WATCHING</span>
+          <br />
+          <Button size="xl" className="rounded-3xl px-16 text-xl">
+            Lets Start
+          </Button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Landing;
