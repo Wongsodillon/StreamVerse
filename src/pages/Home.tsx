54c0dc757f9229440698a82eb64665d34328d816
Marco Dave <marcodave03@gmail.com>
2024-08-03 10:03:48 +0700

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import MainLayout from "@/layouts/MainLayout";
+import Trending from "@/layouts/Trending";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 const Home = () => {
   return (
     <MainLayout>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Trending/>} />
+        </Routes>
+      </BrowserRouter>
     </MainLayout>
   );
 };
 
 export default Home;
+
+
+
