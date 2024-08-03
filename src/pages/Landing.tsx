f6bd7f01961bbae5555b016b0797c27a4d1d699b
Dillon <81748327+Wongsodillon@users.noreply.github.com>
2024-08-03 18:38:27 +0700

    sdlfkmsdlk

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,20 +3,29 @@ import { Button } from "@/components/ui/button";
 import PurpleShape from "../assets/purple_shape.png";
 import Rocket from "../assets/rocket.png";
 import PurpleSphere from "../assets/purple_sphere.png";
+import YellowSphere from "../assets/yellow_sphere.png";
 import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen relative home-background">
+    <div className="h-screen relative home-background no-scrollbar overflow-y-auto scroll-smooth">
       <div className="w-64 h-[44rem] border absolute top-0 drop-shadow-2xl rounded-br-full rounded-bl-full z-0">
         <img src={PurpleShape} className="h-full" />
       </div>
       <div className="w-32 h-32 absolute left-[3%] top-4 hidden md:block">
         <img src={PurpleSphere} alt="" />
       </div>
-      <div className="w-[30%] h-64 border border-white yellow-gradient absolute bottom-0 right-0 drop-shadow-2xl rounded-tl-full rounded-bl-full z-0"></div>
+      <div className="w-16 h-16 absolute left-[15%] top-48 hidden md:block">
+        <img src={PurpleSphere} alt="" />
+      </div>
+      <div className="w-[30%] h-64 border border-white yellow-gradient absolute bottom-0 right-0 drop-shadow-2xl rounded-tl-full rounded-bl-full z-0">
+        <img
+          src={YellowSphere}
+          className="absolute h-16 w-16 hidden md:block"
+        />
+      </div>
       <Navbar />
       <main className="relative flex flex-col-reverse items-center justify-center text-center pt-32 md:flex-row">
         <div className="lg:absolute w-[30rem] h-[30rem] left-0 top-[50%]">
@@ -30,7 +39,11 @@ const Landing = () => {
             WATCHING
           </span>
           <br />
-          <Button size="xl" className="rounded-3xl px-16 text-xl" onClick={() => navigate('/home')}>
+          <Button
+            size="xl"
+            className="rounded-3xl px-16 text-xl"
+            onClick={() => navigate("/home")}
+          >
             Lets Start
           </Button>
         </div>
