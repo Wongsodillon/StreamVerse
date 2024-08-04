0cb29fc2c8933ecf8c00afb5600bc9c4e7e8a263
Marco Dave <marcodave03@gmail.com>
2024-08-04 20:28:59 +0700

    .

--- src/layouts/Trending.tsx
+++ src/layouts/Trending.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState, useRef } from 'react';
-import Place1 from '@/assets/place1.jpg';
+import React, { useState, useRef } from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import Place1 from '@/assets/place1.webp';
 import Place2 from '@/assets/place2.jpg';
 import Place3 from '@/assets/place3.jpg';
 import Place4 from '@/assets/place4.jpg';
+import Thumbnail from '@/components/Thumbnail';
 import '@/layouts/page.css';
 
 const Dashboard: React.FC = () => {
@@ -23,21 +26,19 @@ const Dashboard: React.FC = () => {
     setCurrentIndex(newIndex);
   };
 
-  useEffect(() => {
-    const handleNextClick = () => showSlider('next');
-    const handlePrevClick = () => showSlider('prev');
+  const carouselRef = useRef<HTMLDivElement>(null);
 
-    const nextButton = nextRef.current;
-    const prevButton = prevRef.current;
+  const scrollLeft = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: -150, behavior: 'smooth' });
+    }
+  };
 
-    if (nextButton) nextButton.addEventListener('click', handleNextClick);
-    if (prevButton) prevButton.addEventListener('click', handlePrevClick);
-
-    return () => {
-      if (nextButton) nextButton.removeEventListener('click', handleNextClick);
-      if (prevButton) prevButton.removeEventListener('click', handlePrevClick);
-    };
-  }, [currentIndex]);
+  const scrollRight = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: 150, behavior: 'smooth' });
+    }
+  };
 
   return (
     <div>
@@ -45,92 +46,97 @@ const Dashboard: React.FC = () => {
         <section className="m-0 p-0 pb-20">
           <div className="flex flex-wrap mx-auto ">
             <div className="w-full">
-              <div className="relative w-[80vw] h-[60vh] mt-0 p-0 carousel">
+              {/* carousel */}
+              <div className="relative h-[60vh] sm:h[100vh] mt-0 p-0 carousel">
                 <div className="list">
-                  <div className={currentIndex === 0 ? 'item active' : 'item'}>
-                    <img src={Place1}></img>
-                    <div className="content">
-                      <div className="author">Popular Places</div>
-                      <div className="title">TAKA</div>
-                      <div className="title">MAKASSAR</div>
-                      <div className="topic">ISLAND </div>
+                  {/* slide */}
+                  <div className={currentIndex === 0 ? 'item active' : 'absoulte inset-0 w-full h-full'}>
+                    <img className="w-full h-full object-cover overflow-hidden" src={Place1}></img>
+                    <div className="absolute w-[90%] max-w-full left-1/2 transform -translate-x-1/2 box-border text-white " style={{ paddingRight:"50%", paddingLeft:"1%", top:"40%"}}>
+                      <div style={{fontWeight:"bold", letterSpacing:"2px"}}>Game Category</div>
+                      <div  className="title" style={{fontSize:"3.5em", fontWeight:"bold", lineHeight:"1.3em"}}>James Dillon </div>
                       <div className="des" style={{fontSize:20}}>  
-                        Dive into the azure waters surrounding Takam Makassar, a picturesque sandbar in the heart of Komodo National Park. Snorkel alongside vibrant coral reefs and an array of tropical fish, or simply bask in the sun on this pristine stretch of sand.
+                        1 on 1 Death Match Valorant Tournament
                       </div>
-                      <div className="buttons"></div>
+                      <div>
+                        <div className="flex pt-2 items-center">
+                          <Avatar className="w-14 h-14">
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                          </Avatar>
+                          <div className="p-4">James Dillon Gaming </div>
+                        </div>
+                      </div>
+                      <div className="pt-2">
+                        <Button className="w-1/3" variant={"secondary"} >Watch</Button>
+                      </div>
                     </div>
                   </div>
-                  <div className={currentIndex === 1 ? 'item active' : 'item'}>
-                    <img src={Place2}></img>
-                    <div className="content">
-                      <div className="title">KOMODO</div>
-                      <div className="title">DRAGON</div>
-                      <div className="topic">ISLAND</div>
-                      <div className="des">
-                        Encounter the legendary Komodo dragons in their natural habitat on Komodo Island. Trek through rugged landscapes and witness these ancient creatures up close, amidst a backdrop of breathtaking scenery and diverse wildlife.
+                  <div className={currentIndex === 1 ? 'item active' : 'absoulte inset-0 w-full h-full'}>
+                    <img className="w-full h-full object-cover overflow-hidden" src={Place2}></img>
+                    <div className="content absolute w-[90%] max-w-full left-1/2 transform -translate-x-1/2 box-border text-white " style={{ paddingRight:"50%", paddingLeft:"1%", top:"40%"}}>
+                      <div style={{fontWeight:"bold", letterSpacing:"2px"}}>Game Category</div>
+                      <div className="title" style={{fontSize:"3.5em", fontWeight:"bold", lineHeight:"1.3em"}}>James Dillon </div>
+                      <div className="des" style={{fontSize:20}}>  
+                        1 on 1 Death Match Valorant Tournament.
                       </div>
-                      <div className="buttons"></div>
                     </div>
                   </div>
-                  <div className={currentIndex === 2 ? 'item active' : 'item'}>
-                    <img src={Place3}></img>
-                    <div className="content">
-                      <div className="title">PADAR</div>
-                      <div className="title">ISLAND</div>
-                      <div className="topic">SCENERY</div>
-                      <div className="des">
-                        Ascend to the summit of Padar Island for unparalleled panoramic views of the Komodo archipelago. Marvel at the contrasting landscapes of rugged cliffs, lush greenery, and pink-hued beaches, offering the perfect backdrop for memorable photographs and unforgettable moments.
+                  <div className={currentIndex === 2 ? 'item active' : 'absoulte inset-0 w-full h-full'}>
+                    <img className='w-full h-full object-cover overflow-hidden' src={Place3}></img>
+                    <div className="content absolute w-[90%] max-w-full left-1/2 transform -translate-x-1/2 box-border text-white " style={{ paddingRight:"50%", paddingLeft:"1%", top:"40%"}}>
+                      <div style={{fontWeight:"bold", letterSpacing:"2px"}}>Game Category</div>
+                      <div className="title" style={{fontSize:"3.5em", fontWeight:"bold", lineHeight:"1.3em"}}>James Dillon </div>
+                      <div className="des" style={{fontSize:20}}>  
+                        1 on 1 Death Match Valorant Tournament
                       </div>
-                      <div className="buttons"></div>
                     </div>
                   </div>
-                  <div className={currentIndex === 3 ? 'item active' : 'item'}>
-                    <img src={Place4}></img>
-                    <div className="content">
-                      <div className="title">PINK</div>
-                      <div className="title">BEACH</div>
-                      <div className="topic">PARADISE</div>
-                      <div className="des">
-                        Experience the enchanting allure of Pink Beach, where the sand takes on a rosy hue, creating a surreal and captivating landscape. Snorkel amidst vibrant coral gardens teeming with marine life, soak up the sun on soft pink sands, and immerse yourself in the tranquility of this idyllic paradise.
+                  <div className={currentIndex === 3 ? 'item active' : 'absoulte inset-0 w-full h-full'}>
+                    <img className ="w-full h-full object-cover overflow-hidden" src={Place4}></img>
+                    <div className="content absolute w-[90%] max-w-full left-1/2 transform -translate-x-1/2 box-border text-white  " style={{ paddingRight:"50%", paddingLeft:"1%", top:"40%"}}>
+                      <div style={{fontWeight:"bold", letterSpacing:"2px"}}>Game Category</div>
+                      <div  className="title" style={{fontSize:"3.5em", fontWeight:"bold", lineHeight:"1.3em"}}>James Dillon </div>
+                      <div className="des" style={{fontSize:20}}>  
+                        1 on 1 Death Match Valorant Tournament
                       </div>
-                      <div className="buttons"></div>
                     </div>
                   </div>
                 </div>
+                {/* thumbnail */}
+
+
+
                 <div className="thumbnail">
                   <div className={currentIndex === 0 ? 'item active' : 'item'} onClick={() => handleThumbnailClick(0)}>
-                    <img src={Place1}></img>
-                    <div className="content">
-                      <div className="title">Taka</div>
-                      <div className="description">Makassar</div>
+                    <img className="w-full h-full object-cover rounded-xl" src={Place1}></img>
+                    <div className="absolute text-white bottom-4 left-4 right-2">
+                      <div className="title" style={{fontWeight:"700"}}>Taka</div>
                     </div>
                   </div>
                   <div className={currentIndex === 1 ? 'item active' : 'item'} onClick={() => handleThumbnailClick(1)}>
-                    <img src={Place2}></img>
-                    <div className="content">
-                      <div className="title">Komodo</div>
-                      <div className="description">Island</div>
+                    <img className="w-full h-full object-cover rounded-xl" src={Place2}></img>
+                    <div className="absolute text-white bottom-4 left-4 right-2">
+                      <div className="title" style={{fontWeight:"700"}}>Komodo</div>
                     </div>
                   </div>
                   <div className={currentIndex === 2 ? 'item active' : 'item'} onClick={() => handleThumbnailClick(2)}>
-                    <img src={Place3}></img>
-                    <div className="content">
-                      <div className="title">Padar</div>
-                      <div className="description">Island</div>
+                    <img className="w-full h-full object-cover rounded-xl" src={Place3}></img>
+                    <div className="absolute text-white bottom-4 left-4 right-2">
+                      <div className="title" style={{fontWeight:"700"}}>Padar</div>
                     </div>
                   </div>
                   <div className={currentIndex === 3 ? 'item active' : 'item'} onClick={() => handleThumbnailClick(3)}>
-                    <img src={Place4}></img>
-                    <div className="content">
-                      <div className="title">Pink</div>
-                      <div className="description">Beach</div>
+                    <img className="w-full h-full object-cover rounded-xl" src={Place4}></img>
+                    <div className="absolute text-white bottom-4 left-4 right-2">
+                      <div className="title" style={{fontWeight:"700"}}>Pink</div>
                     </div>
                   </div>
-                </div>
-                <div className="arrows">
-                  <button ref={prevRef} id="prev">{'<'}</button>
-                  <button ref={nextRef} id="prev">{'>'}</button>
-                </div>
+                </div>  
+                
+                <Thumbnail/>
+
+
               </div>
             </div>
           </div>
