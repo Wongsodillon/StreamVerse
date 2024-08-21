6e033a1aee4e2d1db69160201191db85c30a0f4d
Marco Dave <113909971+Marcodave03@users.noreply.github.com>
2024-08-21 07:25:46 +0700

    Update webSocket.ts

--- src/lib/webSocket.ts
+++ src/lib/webSocket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5555", {
+const socket = io("https://streamverse-hedera-backend.onrender.com", {
   transports: ["websocket", "polling"],
 });
 
